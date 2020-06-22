f1dd39e86714137e7c47a094c0a5ba9a4d0bd17c
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-06-22 09:16:40 -0300

    Created Footer component folder

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+// Icons
+import { FaCopyright } from 'react-icons/fa'
+
+function Footer() {
+    return <p><FaCopyright /> Gabriel Cavalcante 2020</p>
+}
+
+export default Footer
