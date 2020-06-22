c6ae8273aff927b2c4894e81222a7a470f20fa79
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-06-22 09:16:02 -0300

    Created base structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { Route, Switch } from 'react-router-dom'
+
+// Components
+import Header from 'components/Header'
+import Footer from 'components/Footer'
+
+// CSS styles
 import './App.css'
 
 function App() {
-  return <p>App file</p>
+  return (
+    <div>
+      <Header />
+      <Switch>
+        {/* <Route exact path="/" component={Menu}/> */}
+        {/* <Route exact path="/pvp" component={PvpMenu}/> */}
+        {/* <Route exact path="/pve" component={PveMenu}/> */}
+        {/* <Route exact path="/game" component={Game}/> */}
+        {/* <Route exact path="/game/results" component={GameResults}/> */}
+      </Switch>
+      <Footer />
+    </div>
+  )
 }
 
 export default App
