8ef40e2a986b95b3a0acd208f55be52826fafb9f
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-06-22 11:31:54 -0300

    Created Controls UI Component

---
+++ src/components/UI/Controls/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+// Components
+import Control from './Control'
+
+// CSS styles
+import './styles.css'
+
+interface Props {
+    numberOfControls: number,
+    controlConfig: {
+        name: string,
+        onclick: (...params: any[]) => void
+    }[]
+}
+
+function Controls(props: Props) {
+
+    const controls = []
+
+    for (let i = 0; i < props.numberOfControls; i++) {
+        controls.push(
+            <Control
+                key={i}
+                name={props.controlConfig[i].name}
+                onclick={props.controlConfig[i].onclick}
+            />
+        )
+    }
+
+    return (
+        <div className="Controls">
+            {controls}
+        </div>
+    )
+}
+
+export default Controls
