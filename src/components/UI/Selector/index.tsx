ce0574d58a2a2b80befe08a6f79a69fae4d3f08e
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-06-23 15:47:45 -0300

    Created Selector logic

--- src/components/UI/Selector/index.tsx
+++ src/components/UI/Selector/index.tsx
@@ -1,11 +1,16 @@
 import React, { useState, useEffect } from 'react'
 import { FaCaretDown } from 'react-icons/fa'
 
+// CSS styles
+import './styles.css'
+
+interface Option {
+    value: string,
+    label: string
+}
+
 interface Options {
-    options: {
-        value: string,
-        label: string
-    }[],
+    options: Option[],
     defaultValue: string
 }
 
@@ -14,7 +19,9 @@ function Selector(props: Options) {
     const defaultOption = props.options
         .filter(option => option.value === props.defaultValue)[0]
 
-    const [value, setValue] = useState<string>(defaultOption.value)
+    const [label, setLabel] = useState(defaultOption.label)
+    const [value, setValue] = useState(defaultOption.value)
+    const [openCaret, setOpenCaret] = useState(false)
 
     useEffect(() => {
         console.log(value)
@@ -24,19 +31,31 @@ function Selector(props: Options) {
         */
     }, [value/*, props.gameState */])
 
+    function optionSelected(option: Option) {
+        setLabel(option.label)
+        setValue(option.value)
+        setOpenCaret(false)
+    }
+
     return (
         <div className="Selector">
-            <div className="Selected">
-                { defaultOption.label }
+            <div className="Selected" onClick={() => setOpenCaret(!openCaret)}>
+                { label }
                 <FaCaretDown />
             </div>
 
-            <div className="Options">
-                {props.options.map(option => (
-                    <div key={option.value} className="Option" onClick={()=>setValue(option.value)}>
-                        { option.label }
-                    </div>
-                ))}
+            <div className={["Options", openCaret ? "Open": ""].join(' ')}>
+                {
+                    props.options.map(option => (
+                        <div
+                            key={option.value}
+                            className={
+                                ["Option", option.value === value && "Current"].join(' ')
+                            }
+                            onClick={() => optionSelected(option)}
+                        >{ option.label }</div>
+                    ))
+                }
             </div>
         </div>
     )
