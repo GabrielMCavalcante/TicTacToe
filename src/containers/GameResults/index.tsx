6820f0164eab389271a608d73df62b9e46fa0ab5
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-06-23 16:50:59 -0300

    Created GameResults container component logic

--- src/containers/GameResults/index.tsx
+++ src/containers/GameResults/index.tsx
@@ -1,9 +1,42 @@
 import React from 'react'
+import { RouteChildrenProps } from 'react-router-dom'
 
-function GameResults() {
+// Components
+import Controls from 'components/UI/Controls'
+
+// CSS styles
+import './styles.css'
+
+function GameResults(props: RouteChildrenProps) {
+
+    const resultOptionsConfig = [
+        { name: 'Play again', onclick: () => props.history.push('/game') },
+        { name: 'Reset scoreboard', onclick: () => { console.log('Reset Score') } },
+        { name: 'Return to menu', onclick: () => props.history.push('/') }
+    ]
+
     return (
-        <div>
-            
+        <div className="GameResults">
+            <div className="Results">
+                <div className="ResultField">
+                    <p className="FieldTitle">Player 1 - <span>X</span></p>
+                    <p className="FieldContent">3 victories</p>
+                </div>
+
+                <div className="FieldSeparator"></div>
+                
+                <div className="ResultField">
+                    <p className="FieldTitle">Player 2 - <span>O</span></p>
+                    <p className="FieldContent">1 victories</p>
+                </div>
+            </div>
+
+            <div className="Options">
+                <Controls 
+                    numberOfControls={resultOptionsConfig.length}
+                    controlConfig={resultOptionsConfig}
+                />
+            </div>
         </div>
     )
 }
