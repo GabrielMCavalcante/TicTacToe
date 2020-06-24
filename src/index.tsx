88da6ec245232c1e6b8e2650b4b7c6828eb112d9
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-06-24 12:38:24 -0300

    Added redux configuration

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,21 @@ import './index.css'
 import App from './containers/App'
 import * as serviceWorker from './serviceWorker'
 import { BrowserRouter } from 'react-router-dom'
+import thunk from 'redux-thunk'
+import { createStore, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import { Provider } from 'react-redux'
+import GameLogicReducer from 'store/reducers/game-logic'
+
+const store = createStore(GameLogicReducer, composeWithDevTools(applyMiddleware(thunk)))
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 )
