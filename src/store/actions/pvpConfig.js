9615e2e7c133e8fabbc04ba50dfab3c80bbe9b64
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-06-24 17:19:45 -0300

    Removed unused action creator

--- src/store/actions/pvpConfig.js
+++
@@ -1,9 +0,0 @@
-import ActionTypes from './actionTypes'
-
-const actions = {
-    setGameConfig: config => ({ type: ActionTypes.START_GAME, gameConfig: config })
-}
-
-export default {
-    onSetGameConfig: config => dispatch => dispatch(actions.setGameConfig(config))
-}
