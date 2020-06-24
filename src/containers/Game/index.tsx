0c2084d77e62d89edab065d82534aa6f274dcd3a
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-06-24 15:54:27 -0300

    Added redux connection

--- src/containers/Game/index.tsx
+++ src/containers/Game/index.tsx
@@ -1,5 +1,14 @@
-import React from 'react'
-import { RouteChildrenProps } from 'react-router-dom'
+import React, { useEffect } from 'react'
+
+// Redux connection
+import { connect } from 'react-redux'
+
+// Actions
+import actions from 'store/actions/game'
+import global from 'store/actions/global'
+
+// Interfaces
+import StoreState from 'interfaces/store-state'
 
 // Components
 import Controls from 'components/UI/Controls'
@@ -7,17 +16,56 @@ import Controls from 'components/UI/Controls'
 // CSS styles
 import './styles.css'
 
-function Game(props: RouteChildrenProps) {
+function Game(props: any) {
+
+    const { 
+        history,
+        gameState, 
+        onRestartGame,
+        onReturnToMenu,
+    } = props
 
     const gameOptionsConfig = [
-        { name: 'Restart Game', onclick: () => {console.log('Restart Game')} },
-        { name: 'Return to Menu', onclick: () => {props.history.push('/')} }
+        { name: 'Restart Game', onclick: () => onRestartGame() },
+        {
+            name: 'Return to Menu', onclick: () => {
+                onReturnToMenu()
+                history.push('/')
+            }
+        }
     ]
+
+    useEffect(() => {
+        if (gameState !== 'playing') {
+            onReturnToMenu()
+            history.push('/')
+        }
+    }, [gameState, onReturnToMenu, history])
+
+    let playerTurn = 'Player 1'
+
+    switch(props.currentPlayer) {
+        case 'player1':
+            {
+                playerTurn = 'Player 1'
+                break
+            }
+        case 'player2':
+            {
+                playerTurn = 'Player 2'
+                break
+            }
+        case 'computer':
+            {
+                playerTurn = 'Computer'
+                break
+            }
+    }
 
     return (
         <div className="Game">
             <div className="GameInfo">
-                <p>Player 1 Turn - X</p>
+                <p>{ playerTurn } Turn - { props.currentTile }</p>
             </div>
 
             <div className="GameOptions">
@@ -30,4 +78,19 @@ function Game(props: RouteChildrenProps) {
     )
 }
 
-export default Game
+function mapStateToProps(state: StoreState) {
+    return {
+        gameState: state.gameState,
+        currentPlayer: state.currentPlayer,
+        currentTile: state.currentTile
+    }
+}
+
+function mapDispatchToProps(dispatch: any) {
+    return {
+        onReturnToMenu: () => dispatch(global.onReturnToMenu()),
+        onRestartGame: () => dispatch(actions.onRestart())
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Game)
