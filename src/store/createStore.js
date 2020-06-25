28c8aeae961dbfdec67959c865233634bb6a9fcd
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-06-25 17:25:49 -0300

    Added trace watcher for redux dev tools

--- src/store/createStore.js
+++ src/store/createStore.js
@@ -2,5 +2,9 @@ import thunk from 'redux-thunk'
 import { createStore, applyMiddleware } from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import GameLogicReducer from 'store/reducers/game-logic'
-const store = createStore(GameLogicReducer, composeWithDevTools(applyMiddleware(thunk)))
+import * as gameCreators from 'store/actions/game'
+
+const composeEnhancers = composeWithDevTools({ gameCreators, trace: true, traceLimit: 25 })
+
+const store = createStore(GameLogicReducer, composeEnhancers(applyMiddleware(thunk)))
 export default store
