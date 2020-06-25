b4b49f0738996308de87cee54b94a8a179fa5571
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-06-25 13:02:37 -0300

    Added new actions

--- src/store/actions/global.js
+++ src/store/actions/global.js
@@ -1,9 +1,13 @@
 import ActionTypes from './actionTypes'
 
 const actions = {
-    resetState: () => ({ type: ActionTypes.RESET_STATE })
+    restartGame: () => ({ type: ActionTypes.RESET_GAME }),
+    resetState: () => ({ type: ActionTypes.RESET_STATE }),
+    changeGamestate: newGamestate => ({ type: ActionTypes.CHANGE_GAMESTATE, newGamestate })
 }
 
 export default {
-    onReturnToMenu: () => dispatch => dispatch(actions.resetState())
+    onRestart: () => dispatch => dispatch(actions.restartGame()),
+    onReturnToMenu: () => dispatch => dispatch(actions.resetState()),
+    onChangeGamestate: newGamestate => dispatch => dispatch(actions.changeGamestate(newGamestate))
 }
