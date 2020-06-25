fe6675850eaacb12d20b351765dc70d3872cb3a3
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-06-25 17:26:58 -0300

    Added new global actions

--- src/store/actions/global.js
+++ src/store/actions/global.js
@@ -3,11 +3,13 @@ import ActionTypes from './actionTypes'
 const actions = {
     restartGame: () => ({ type: ActionTypes.RESET_GAME }),
     resetState: () => ({ type: ActionTypes.RESET_STATE }),
-    changeGamestate: newGamestate => ({ type: ActionTypes.CHANGE_GAMESTATE, newGamestate })
+    changeGamestate: newGamestate => ({ type: ActionTypes.CHANGE_GAMESTATE, newGamestate }),
+    changePlayerPlayed: played => ({ type: ActionTypes.CHANGE_PLAYER_PLAYED, played })
 }
 
 export default {
     onRestart: () => dispatch => dispatch(actions.restartGame()),
     onReturnToMenu: () => dispatch => dispatch(actions.resetState()),
-    onChangeGamestate: newGamestate => dispatch => dispatch(actions.changeGamestate(newGamestate))
+    onChangeGamestate: newGamestate => dispatch => dispatch(actions.changeGamestate(newGamestate)),
+    onChangePlayerPlayed: played => dispatch => dispatch(actions.changePlayerPlayed(played))
 }
