9c4325bede74b0a06af5a547954435de88a0d6af
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-07-02 09:17:31 -0300

    Updates

---
+++ precache-manifest.37f6606c1fae3d3eb00eff3aa55ca02e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d042d861e4d922c3ccf69a107e5f04b",
+    "url": "/TicTacToe/index.html"
+  },
+  {
+    "revision": "cd8856352cf0121add01",
+    "url": "/TicTacToe/static/css/main.00c3bada.chunk.css"
+  },
+  {
+    "revision": "8fd5fc84cda790856d8e",
+    "url": "/TicTacToe/static/js/2.5a8c424c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/TicTacToe/static/js/2.5a8c424c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd8856352cf0121add01",
+    "url": "/TicTacToe/static/js/main.f917ab0d.chunk.js"
+  },
+  {
+    "revision": "43541b8edbb023f9c87b",
+    "url": "/TicTacToe/static/js/runtime-main.71adabef.js"
+  }
+]);
